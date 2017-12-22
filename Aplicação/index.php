aa6d5b64ff5a73cb44d9160500f4d5c0c559d838
Thiago Henrique Felix <thiagoacdc12@gmail.com>
2017-12-22 10:51:33 -0200

    release 0.5

--- Aplicação/index.php
+++ Aplicação/index.php
@@ -2,144 +2,153 @@
 <!DOCTYPE HTML>
 <html lang="pt-br">
     <head>
-        
-        <title>Project-X | Salão de Beleza</title>
+
+        <title>La'Rose | Salão de Beleza</title>
         <meta charset="utf-8">
         <meta name="description" content="Salão de cabeleireiros">
         <meta name="keywords" content="Beleza,Salão">
         <meta name="author" content="Thiago Henrique Felix">
-        <meta name="viewport"content="width:device-width, initial-scale=1.0">
-        
+        <meta name="viewport"content="width:device-width, initial-scale=1.">
+
         <!-- CSS -->
-        <link type="text/css" rel="stylesheet" href="css/style.css">
-        
+        <link type="text/css" rel="stylesheet" href="css/style.css?v=2">
+
         <!-- Javascript -->
         <link type="text/javascript" href="js/javascript.js">
-        
+
         <!-- Icons | site = Fonts Awesome -->
         <link rel="stylesheet" type="text/css" href="pacotes/fontawesome-free-5.0.2/web-fonts-with-css/css/fontawesome-all.css">
 
-       
-        
-        
+
+
+
     </head>
     <body>
-        
+
         <div class="cabeçalho" >
-            
-            <div class="logo"><a href="index.php"><h1>Project-X</h1></a></div>
+
+            <div class="logo"><a href="index.php"><h1>La'Rose</h1></a></div>
             <button class="btn-menu"><i class="fa fa-bars fa-lg" ></i></button>
-            
+
             <nav class="menu">
-                
+
                 <a class="fechar" href="#"><i class="fa fa-times fa-lg"></i></a> 
                 <ul>
                     <a href="#"><li> Inicio<i class="fa fa-home"> </i></li> </a> 
                     <a href="#"><li> Quem somos<i class="fa fa-users"> </i></li> </a> 
                     <a href="#"><li> Contato<i class="fa fa-user"> </i></li> </a> 
                 </ul>
-                
+
             </nav>
-              
-            
+
+
         </div> 
-        
+
         <div class="banner">
-            
+
             <h2>Seja Bem-Vindo</h2>
-            <p> <i class="logo-sm">Project-X</i>é um salão de cabeleireiro que atende tanto homens quanto mulheres <br/>Venha nos Conhecer</p>
-            
+            <p> <i class="logo-sm">La'Rose</i>é um salão de cabeleireiro que atende tanto homens quanto mulheres <br/>Venha nos Conhecer</p>
+
         </div>
-        
+
         <main class="conteiner-equipe">
-            
+
             <div class="texto">
                 <h3>Nossa Equipe</h3>
                 <p>A nossa equipe é composta por 3 pessoas muito talentosas, temos a missão de embelezar o dia de todas as pessoas</p>
             </div>
-            
-            <article class="perfil" >
-                <div class="foto">
-                    <img alt="Foto da Angelica da Silva Benegathi" src="img/mulher-perfil.jpg?v=2">
+
+            <div class="conteiner-perfils">
+                
+                <div class="conteiner-perfil">
+                    <article class="perfil" >
+                    <div class="foto">
+                        <img alt="Foto da Angelica da Silva Benegathi" src="img/mulher-perfil.jpg?v=2">
+                    </div>
+                    <div class="nome">Angelica da Silva Benegathi</div>
+                    <div class="funcao">Cabeleireira</div>
+                </article>
                 </div>
-                <div class="nome">Angelica da Silva Benegathi</div>
-                <div class="funcao">Cabeleireira</div>
-            </article>
-            
-            <article class="perfil" >
-                <div class="foto">
-                    <img alt="Foto da Beatriz Ferreira Bertoli" src="img/mulher-perfil_2.jpg">
+                <div class="conteiner-perfil">
+                     <article class="perfil" >
+                    <div class="foto">
+                        <img alt="Foto da Beatriz Ferreira Bertoli" src="img/mulher-perfil_2.jpg">
+                    </div>
+                    <div class="nome">Beatriz Ferreira Bertoli</div>
+                    <div class="funcao">Sócia</div>
+                </article>
                 </div>
-                <div class="nome">Beatriz Ferreira Bertoli</div>
-                <div class="funcao">Sócia</div>
-            </article>
-            
-            <article class="perfil" >
-                <div class="foto">
-                    <img alt="Foto do Reginaldo da Cruz Mancin" src="img/homem-perfil.jpg">
+               
+                <div class="conteiner-perfil">
+                     <article class="perfil" >
+                    <div class="foto">
+                        <img alt="Foto do Reginaldo da Cruz Mancin" src="img/homem-perfil.jpg">
+                    </div>
+                    <div class="nome">Reginaldo da Cruz Mancin</div>
+                    <div class="funcao">Escritório</div>
+                </article>
                 </div>
-                <div class="nome">Reginaldo da Cruz Mancin</div>
-                <div class="funcao">Escritório</div>
-            </article>
-            
+               
+            </div>
+
         </main>
-        
-        <div class="contato">
-            
+
+        <section class="contato">
+
             <form>
                 <h3>Entre em contato conosco</h3>
                 <input class="caixa-texto" type="text" maxlength="20" name="titulo" placeholder="Titulo..." > 
                 <input class="caixa-texto" type="email" maxlength="30" name="email" placeholder="Seu e-mail..." > 
                 <textarea class="caixa-texto textarea" type="texto" maxlength="500" name="texto" placeholder="Escreva o texto..." ></textarea> 
                 <button class="btn-contato" >Enviar</button>
-                
+
             </form>
-            
-        </div>
-        
+
+        </section>
+
         <footer class="rodape">
-            
+
             <div class="redes-sociais">
                 <ul>
-                    
-                    <li ><a href="#"></a><i class="fa fa-comment" aria-hidden="true"></i></li>
-                    <li ><a href="#"></a><i class="fa fa-copyright fa-lg"></i></li>
-                    <li ><a href="#"></a><i class="fa fa-car fa-lg"></i></li>
-                    <li ><a href="#"></a><i class="fa fa-book fa-lg"></i></li>
+
+                    <li ><a href="#"></a><i class="fa fa-facebook" aria-hidden="true"></i></li>
+                    <li ><a href="#"></a><i class="fa fa-twitter" aria-hidden="true"></i></li>
+                    <li ><a href="#"></a><i class="fa fa-google" aria-hidden="true"></i></li>
+                    <li ><a href="#"></a><i class="fa fa-instagram" aria-hidden="true"></i></li>
                 </ul>
             </div>
-            
+
             <div class="direitos-autorais">
-                <p>Copyright © 2017, Project-X. Todos os direitos reservados.</p>
+                <p>Copyright © 2017, La'Rose. Todos os direitos reservados.</p>
             </div>
-            
-            
+
+
         </footer>
-        
-        
-        
+
+
+
     </body>
-    
-    
+
+
     <!-- Scripts -->
     <script type="text/javascript" src="pacotes/jquery/jquery-3.2.1.js"></script>
-    
-    
+
+
     <script type="text/javascript">
-    
-        $(".btn-menu").click(function(){
-           $(".menu").show("slow"); 
-           $(".btn-menu").hide(600);
-           $(".fechar").show(600);
+
+        $(".btn-menu").click(function () {
+            $(".menu").show("fast");
+            $(".btn-menu").hide("fast");
+            $(".fechar").show("fast");
         });
-        
-        $(".fechar").click(function(){
-           $(".menu").hide("slow");
-           $(".btn-menu").show(600);
-           $(".fechar").hide(600);
+
+        $(".fechar").click(function () {
+            $(".menu").hide("fast");
+            $(".btn-menu").show("fast");
+            $(".fechar").hide("fast");
         });
-    
-    
+
+
     </script>
-    
+
 </html>
